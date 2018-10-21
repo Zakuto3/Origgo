4cd3d34e7f7dd2de1fcb5514a0cba21367d36880
Zakuto <zakuto3@gmail.com>
2018-10-21 23:42:16 +0200

    Added PostReq from client. Added DBconnection in Serverside

--- src/serverside.js
+++ src/serverside.js
@@ -22,7 +22,33 @@ app.use(function (req, res, next) {
     next();
 });
 
+//testing connection, sending a query to DB, This is gonna be wrapped in post request below
+let DBresult = "";
+connection.connect(); 
+connection.query('SELECT name FROM origgo.tesssst WHERE id = 1;',  (error, results, fields) => {
+  if (error) throw error;
+  console.log('result from Database: on "SELECT name FROM origgo.tesssst WHERE id = 1;": ', results[0].name);
+  DBresult = results[0].name;
+});
+connection.end();
 
+//Post awaint requests, DB connection should be inside this but separeted for better understanding
+let login = false;
+app.post('/request',(req, res) =>{
+  login = true;
+  console.log("req.body: ",req.body,"req.query: ",req.query); 
+  console.log("login: ",login);
+  res.send(DBresult) //sends DB result,
+});
+
+//request to go to map site, only allowed if loged in
+app.get('/map.html', (req, res) => {
+  if (login) {
+    res.send(fs.readFileSync('../public/map.html', 'utf8'));
+  }else{
+    res.send(fs.readFileSync('../public/index.html', 'utf8'));
+  }
+});
 
 app.use(express.static(__dirname + '/../public')); // if not this is given, give specific adress like : app.get('/', (req, res) => res.send(fs.readFileSync('./index.html', 'utf8')));
 
