d812078d3174574265ee016d56a1a4f91ec4a6c2
Zakuto <zakuto3@gmail.com>
2018-10-21 23:34:58 +0200

    added DBinfo

---
+++ src/DBinfo.js
@@ -0,0 +1,10 @@
+//info of database connection
+module.exports = {
+  connectionstring : {
+  host    	: '******',
+  port	   	: '*****',
+  user 	   	: '******',
+  password 	: '******',
+  database	: '******'
+	}
+};
