98e56b6eb91ceb2154fe82afc3226d647efe0887
Lennard <l.niesig@googlemail.com>
2018-12-11 19:17:55 +0100

    befor merge

--- public/Js/Script.js
+++ public/Js/Script.js
@@ -101,20 +101,3 @@ function AJAXget(link, callback){
     }
     req.send();
 }
-
-
-
-//image upload
-
-var formidble = require(formidble);
-
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-}).listen(8080);
